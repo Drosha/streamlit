0efeb1e23ba75e00b39ef8eff47b44b8cfea0121
Andrew <drosha@gmail.com>
2023-08-15 15:09:03 +0300

    test 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import http.client, urllib.parse
 import streamlit as st
 import pandas as pd
- 
-st.write("""
-# My first app
-Hello *world!*
-""")
+
+import plotly.express as px
+
+fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", zoom=3)
+
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+st.plotly_chart(fig)
